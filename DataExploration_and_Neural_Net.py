729ffa0ddd8e6ad62c00c3f5bae8cc9ee58889c2
MilesJoseph <miles.klingenberg@outlook.com>
2020-05-05 11:07:01 -0700

    neural network

--- DataExploration_and_Neural_Net.py
+++ DataExploration_and_Neural_Net.py
@@ -120,12 +120,23 @@ selected_features_be = cols
 print(selected_features_be)
 
 data_1 = data[['OverallQual', 'totalsf', 'GarageCars', 'ExterQual', 'TotalBsmtSF', '1stFlrSF', 'KitchenQual', 'SalePrice']]
+data_1 = data_1.replace([np.inf, -np.inf], np.nan)
+data_1 = data_1.dropna()
+print(data_1['OverallQual'].max())
+#print(data_1.isnull().any())
 data_1 = np.array(data)
+
+
 
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.pipeline import Pipeline
+import tensorflow as tf
+import keras.backend as kb
+import keras
+from keras.optimizers import SGD
 
 
 x = data_1[:,0:7]
@@ -139,16 +150,16 @@ print(scaler_y.fit(y))
 yscale=scaler_y.transform(y)
 
 model = Sequential()
-model.add(Dense(3, input_dim=7, kernel_initializer='normal', activation='relu'))
-model.add(Dense(2, activation='relu'))
+model.add(Dense(12, input_dim=7, kernel_initializer='normal', activation='relu'))
+model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='linear'))
 model.summary()
 
-model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mse', 'mae'])
-history = model.fit(xscale, yscale, epochs = 150, batch_size = 50, verbose = 1)
+model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
+history = model.fit(x, y, epochs=150, batch_size=50,  verbose=1)
+
 
 test_ = pd.read_csv("/Users/milesklingenberg/Documents/UWMSBA/590/Data/test_house-1.csv", dtype=CAT_DTYPES)
-
 
 SFTotals_test = test_.filter(regex="SF")
 test_['totalsf_test'] = SFTotals_test.sum(axis=1)
@@ -161,14 +172,14 @@ test_["qualttotal_test"] = QualTotal_test.sum(axis=1)
 data_2 = test_[['Overall Qual', 'totalsf_test', 'Garage Cars', 'Exter Qual', 'Total Bsmt SF', '1st Flr SF', 'Kitchen Qual']]
 data_2['Kitchen Qual'] = data_2['Kitchen Qual'].astype('category').cat.codes
 data_2['Exter Qual'] = data_2['Exter Qual'].astype('category').cat.codes
-data_2 = data_2.dropna()
+
+
 data_3 = np.array(data_2)
 pd.set_option('display.max_columns', 500)
 
-Xnew= scaler_x.transform(data_3)
+x_test = data_3[:,0:7]
+Xnew= scaler_x.transform(x_test)
 ynew= model.predict(Xnew)
-#invert normalize
 ynew = scaler_y.inverse_transform(ynew)
 Xnew = scaler_x.inverse_transform(Xnew)
-
-)
+print(ynew)
